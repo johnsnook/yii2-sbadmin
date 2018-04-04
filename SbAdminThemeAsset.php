b444b97494f1a02956d8849bdd0a716038afc61e
John Snook <jsnook@gmail.com>
2018-04-03 22:29:42 -0400

    testin

--- SbAdminThemeAsset.php
+++ SbAdminThemeAsset.php
@@ -9,19 +9,22 @@ use yii\web\AssetBundle;
  */
 class SbAdminThemeAsset extends AssetBundle {
 
-    public $sourcePath = __DIR__ . '/sb-admin';
+    public $sourcePath = __DIR__;
     public $css = [
-        'css/sb-admin' . (YII_ENV_DEV ? '' : '.min') . '.css',
+        'sb-admin/css/sb-admin' . (YII_ENV_DEV ? '' : '.min') . '.css',
+        #'assets/css/yii2-sbadmin.css',
+        '@vendor/rmrevin/yii2-fontawesome/assets/web-fonts-with-css/css/fa-solid.min.css',
     ];
     public $js = [
-        'vendor/bootstrap/js/bootstrap.bundle' . (YII_ENV_DEV ? '' : '.min') . '.js',
-        'vendor/jquery-easing/jquery.easing' . (YII_ENV_DEV ? '' : '.min') . '.js',
-        'js/sb-admin' . (YII_ENV_DEV ? '' : '.min') . '.js'
+        'sb-admin/vendor/bootstrap/js/bootstrap.bundle' . (YII_ENV_DEV ? '' : '.min') . '.js',
+        'sb-admin/vendor/jquery-easing/jquery.easing' . (YII_ENV_DEV ? '' : '.min') . '.js',
+        'sb-admin/js/sb-admin' . (YII_ENV_DEV ? '' : '.min') . '.js'
     ];
     public $jsOptions = ['position' => \yii\web\View::POS_END];
     public $depends = [
         'yii\web\JqueryAsset',
-        'yii\bootstrap\BootstrapAsset'
+        'yii\bootstrap\BootstrapAsset',
+            #'rmrevin\yii\fontawesome\AssetBundle',
     ];
     public $publishOptions = ['forceCopy' => YII_ENV_DEV ? true : false];
 
